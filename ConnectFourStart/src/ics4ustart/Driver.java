0a7153dc155d4e8aede44f017e4629692facc517
333318319 <333318319@C-2UA310106S.tdsb.on.ca>
2019-05-02 14:28:18 -0400

    Updated Driver

--- ConnectFourStart/src/ics4ustart/Driver.java
+++ ConnectFourStart/src/ics4ustart/Driver.java
@@ -9,8 +9,8 @@ public class Driver {
 	public static void main(String[] args) throws InterruptedException {
 
 		// Setup constants for the Board
-		final int ROWS = 8;
-		final int COLS = 8;
+		final int ROWS = 9;
+		final int COLS = 9;
 
 		// create the board
 		Board board = new Board(ROWS, COLS);
